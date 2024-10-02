b741188b34a3122a614afe41a545c251aac970ee
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-10-02 10:21:16 -0500

    Update App.py

--- App.py
+++ App.py
@@ -1,3 +1,4 @@
+import paho.mqtt.client as paho
 import tensorflow as tf
 from PIL import Image, ImageOps
 import numpy as np
@@ -5,7 +6,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
-import paho.mqtt.client as paho
+
 
 # MQTT
 def predictDigit(image):
